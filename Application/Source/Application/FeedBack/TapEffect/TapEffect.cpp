13d55b323b92472bb202b6a19724ef6687bba2b3
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2025-07-29 10:08:33 +0900

    TrigerEffectを分離してtapじにエフェクトが出るようにした
    - `TapEffect`に関連するソースファイルとヘッダーファイルを追加
    - `FeedbackEffect`クラスに`TapEffect`のインスタンスを追加し、初期化と再生を実装
    - `JudgeEffect`、`JudgeSound`、`JudgeText`のフィルターを新しい階層`Application\FeedBack\Judge`に整理
    - 古い`TapEffects`フィルターを削除し、新しい構造に合わせて整理
    - `TitleScene`クラスに`TextGenerator`を追加し、テキスト描画機能を強化
    - `SampleFramework`の初期化時に最初のシーンを`Sample`から`TitleScene`に変更

---
+++ Application/Source/Application/FeedBack/TapEffect/TapEffect.cpp
@@ -0,0 +1,18 @@
+#include "TapEffect.h"
+
+#include <Application/Lane/Lane.h>
+#include <Application/Effects/TapEffects/TriggerEffects.h>
+
+void TapEffect::Initialize()
+{
+    TriggerEffects::Initialize();
+}
+
+void TapEffect::Play(int32_t _laneIndex)
+{
+    Vector3  lanePos = Lane::GetLaneEndPosition(_laneIndex); // レーンの開始位置を取得
+
+    // 中心の円を発生させる
+    TriggerEffects::EmitCenterCircles(lanePos);
+
+}
